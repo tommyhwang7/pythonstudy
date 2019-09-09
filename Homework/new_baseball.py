ade6dc9fae89265a718eac72f112c86c360be192
tommyhwang7 <sh82h37@naver.com>
2019-09-09 21:46:25 +0900

    remove unused code

--- Homework/new_baseball.py
+++ Homework/new_baseball.py
@@ -13,23 +13,7 @@ del Random_num_list [First_num]
 
 print (Random_num_list)
 
-'''count=0
 
-while True:
-    count = count+1
-    if count>10000:
-        print ('error')
-        break
-
-    Random_A=random_num()
-    Random_B=random_num()
-    Random_C=random_num()    
-    
-    if (Random_A != Random_B) and (Random_A != Random_C) and (Random_B != Random_C):
-        Random_number.append(Random_A)   
-        Random_number.append(Random_B)
-        Random_number.append(Random_C)
-        break'''
 
 #print (Random_number)
 
