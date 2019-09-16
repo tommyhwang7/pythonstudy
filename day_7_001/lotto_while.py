1887bb1f16c3b32c93a77dc536375f77581656d3
tommyhwang7 <sh82h37@naver.com>
2019-09-16 23:45:10 +0900

    lotto_for,while update

---
+++ day_7_001/lotto_while.py
@@ -0,0 +1,19 @@
+import random
+
+lotto_count=0
+
+lotto_nums=[]
+
+while True:
+    lotto_count = lotto_count+1
+    if lotto_count>45 :
+        break
+    lotto_nums.append(lotto_count)
+
+print (lotto_nums)
+
+
+
+
+#Random_num = random.randrange(1,10)
+
